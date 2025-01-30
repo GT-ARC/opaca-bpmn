4decd047eaa8b1591486c77910d97b6a30fe7660
Cedric Braun <braun@dai-labor.de>
2025-01-30 10:18:55 +0100

    remove unused file

--- opaca-bpmn-editor/src/simulation/event-based-gateway-handling/index.js
+++
@@ -1,6 +0,0 @@
-import EventBasedGatewayHandler from './EventBasedGatewayHandler';
-
-export default {
-    __init__: [ 'eventBasedGatewayHandler' ],
-    eventBasedGatewayHandler: [ 'type', EventBasedGatewayHandler ]
-};
