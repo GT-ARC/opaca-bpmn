33b49516a643fa5e93ba3a615be049810759d2aa
Cedric Braun <braun@dai-labor.de>
2025-03-10 13:38:30 +0100

    fix userTask simulation-pause

--- opaca-bpmn-editor/src/simulation/interpreter-base/InterpreterBaseModule.js
+++ opaca-bpmn-editor/src/simulation/interpreter-base/InterpreterBaseModule.js
@@ -3,8 +3,8 @@ import {isAny} from "bpmn-js/lib/features/modeling/util/ModelingUtil";
 import {handleEnd, handleStart, initializeVariables} from "./util";
 import {handleConditionalEvents} from "../event-based-gateway-handling/EventBasedGatewayHandler";
 
-export default function InterpreterBase(activationManager,
-    eventBus, elementRegistry, simulationSupport, exclusiveGatewaySettings, inclusiveGatewaySettings) {
+export default function InterpreterBase(activationManager, eventBus, elementRegistry,
+    simulationSupport, exclusiveGatewaySettings, inclusiveGatewaySettings, simulator) {
 
     this._eventBus = eventBus;
     this._elementRegistry = elementRegistry;
@@ -17,7 +17,7 @@ export default function InterpreterBase(activationManager,
     activationManager.registerModule(this);
 
     // ON ELEMENT ENTER
-    eventBus.on('trace.elementEnter', (event) => {
+    eventBus.on('trace.elementEnter', async (event) => {
         // Don't do anything, if process is not executable
         if(!this._isActive){
             return;
@@ -30,14 +30,26 @@ export default function InterpreterBase(activationManager,
         if(is(element, 'bpmn:EventBasedGateway')){
             const triggerElements = getTriggers(element);
             // TODO don't pass simulationSupport like this
-            handleConditionalEvents(triggerElements, scope, this._simulationSupport);
+            await handleConditionalEvents(triggerElements, scope, this._simulationSupport);
+            return;
         }
+
         // Assignments
-        handleStart(element, scope);
+        if(isAny(element, ['bpmn:UserTask', 'bpmn:ServiceTask'])){
+            // Pause at UserTasks and SeviceTasks until their actions are complete
+            simulator.waitAtElement(element);
+            // Await results
+            await handleStart(element, scope);
+            // Trigger to continue simulation
+            simulationSupport.triggerElement(element.id);
+        }else{
+            // No need to pause for other elements
+            await handleStart(element, scope);
+        }
     });
 
     // ON ELEMENT EXIT
-    eventBus.on('trace.elementExit', (event) => {
+    eventBus.on('trace.elementExit', async (event) => {
         // Don't do anything, if process is not executable
         if(!this._isActive){
             return;
@@ -56,7 +68,7 @@ export default function InterpreterBase(activationManager,
             this._inclusiveGatewaySettings.setLive(event.scope);
         }
         // Assignments
-        handleEnd(element, event.scope);
+        await handleEnd(element, event.scope);
     })
 }
 
@@ -82,5 +94,6 @@ InterpreterBase.$inject = [
     'elementRegistry',
     'simulationSupport',
     'exclusiveGatewaySettings',
-    'inclusiveGatewaySettings'
+    'inclusiveGatewaySettings',
+    'simulator'
 ];
